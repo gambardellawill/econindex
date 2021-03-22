5afdee8e94c4630752cca6e8551c5e587215f5f8
William Gambardella <williamcampo@gmail.com>
2021-03-22 14:06:40 -0300

    Organising query functions into the bcb module

--- app.py
+++ app.py
@@ -1,23 +1,6 @@
 #  -*- coding:utf-8 -*-
 
-from zeep import Client, exceptions
-from datetime import date
-
-def selic():
-    return get_last_series(11)
-
-def get_last_series(series_code):
-    today = "13/05/2020"
-    return get_series(series_code, today)
-
-def get_series(series_code, date):
-    client = Client('https://www3.bcb.gov.br/sgspub/JSP/sgsgeral/FachadaWSSGS.wsdl')
-    try:
-        response = client.service.getValor(series_code, date)
-        return response['_value_1']
-    except:
-        print("Nao rolou :/")
+from bcb import timeseries
 
 if __name__ == "__main__":
-    print(selic())
-
+    print(timeseries.selic())
---
+++ bcb/timeseries.py
@@ -0,0 +1,19 @@
+#  -*- coding:utf-8 -*-
+
+from zeep import Client, exceptions
+from datetime import date
+
+def selic():
+    return get_last_series(11)
+
+def get_last_series(series_code):
+    today = "13/05/2020"
+    return get_series(series_code, today)
+
+def get_series(series_code, date):
+    client = Client('https://www3.bcb.gov.br/sgspub/JSP/sgsgeral/FachadaWSSGS.wsdl')
+    try:
+        response = client.service.getValor(series_code, date)
+        return response['_value_1']
+    except:
+        print("Nao rolou :/")
